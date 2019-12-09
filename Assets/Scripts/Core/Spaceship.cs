9d400d641789ff49586ff5230c134fd9c5407d42
chenkenZzz <d512668247@gmail.com>
2019-12-09 16:12:47 +0900

    変更SG

---
+++ Assets/Scripts/Core/Spaceship.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+	private IControlDevice controlDevice;
+	private SpaceshipStatus spaceshipStatus;
+	public SpaceshipStatus SpaceshipStatus
+	{
+		get
+		{
+			return spaceshipStatus;
+		}
+	}
+
+	public void Set(IControlDevice controlDevice,SpaceshipStatus spaceshipStatus)
+	{
+		this.controlDevice = controlDevice;
+		this.spaceshipStatus = spaceshipStatus;
+	}
+
+  
+    void Update()
+    {
+		controlDevice.Execute();
+    }
+}
---
+++ Assets/Scripts/ObjectPool/GameObjectPool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPool
+{
+	private GameObject prefab;
+	private int preCreateNum;
+	private List<GameObject> pool;
+	
+	public GameObjectPool(GameObject prefab,int preCreateNum)
+	{
+		pool = new List<GameObject>();
+		
+
+		this.prefab = prefab;
+		this.preCreateNum = preCreateNum;
+
+		for(var i = 0; i < preCreateNum; ++i)
+		{
+			var go = MonoBehaviour.Instantiate(prefab) as GameObject;
+			go.SetActive(false);
+			pool.Add(go);
+		}
+	}
+
+}
+
