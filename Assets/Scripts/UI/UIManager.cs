a58d1aef457d59c587d94aae2bdcfa0688cc2fe3
chenkenZzz <d512668247@gmail.com>
2020-01-16 16:24:10 +0900

    新しい

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : Singleton<UIManager>
+{
+	private GameObject root;
+
+	private UI_TalkTextHandler textHandler;
+	private GameObject talkPrefab;
+
+	public override void Initialize()
+	{
+		root = GameObject.Find("Canvas") as GameObject;
+
+		talkPrefab = Resources.Load("talkBubblePrefab") as GameObject;
+		textHandler = new UI_TalkTextHandler(root, talkPrefab);
+
+		MsgCenter.Instance.Subscribe(MsgType.MSG_TALK_TO_NPC, MsgHandle);
+	}
+
+	private void MsgHandle(Msg msg)
+	{
+		switch(msg.Type)
+		{
+			case MsgType.MSG_TALK_TO_NPC:
+				var NPC = msg["NPC"] as AbstractNPC;
+				textHandler.SetTalk(NPC.Name,NPC.TalkData, NPC.transform.position);
+				break;
+		}
+	}
+
+	public void Execute()
+	{
+		textHandler.Execute();
+	}
+
+	public override void Destory()
+	{
+		MsgCenter.Instance.Unsubscribe(MsgType.MSG_TALK_TO_NPC, MsgHandle);
+	}
+
+}
---
+++ Assets/Scripts/UI/UI_TalkTextHandler.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_TalkTextHandler
+{
+	private const float readTextMaxTime = 0.1f;
+	private const float lockCoolDownMaxTime = 0.5f;
+
+	private GameObject root;
+	private GameObject talkBubblePrefab;
+	private Image talkUIImage;
+	private Text talkUIText;
+	private string[] talkData;
+
+	private bool isLock = false;
+	private bool isTalkStart = false;
+	private bool isOver = true;
+
+	private int index = 0;
+	private int lineIndex = 0;
+	private float readPerCharTimer = 0;
+
+	private bool isLockCoolDown = false;
+	private float lockCoolDownTimer;
+
+    public UI_TalkTextHandler(GameObject root, GameObject prefab)
+	{
+		this.root = root;
+		talkBubblePrefab = prefab;
+	}
+
+	public void Execute()
+	{
+		if(isTalkStart)
+		{
+			readPerCharTimer += Time.deltaTime;
+
+			if (readPerCharTimer >= readTextMaxTime)
+			{
+				if (lineIndex < talkData[index].Length)
+				{
+					talkUIText.text += talkData[index][lineIndex];
+					lineIndex++;
+					readPerCharTimer = 0f;
+				}
+				else
+				{
+					if (index < talkData.Length - 1)
+					{
+						if (Input.GetKeyDown(KeyCode.Space))
+						{
+							lineIndex = 0;
+							index++;
+							talkUIText.text = "";
+
+							readPerCharTimer = 0f;
+						}
+					}
+					else
+					{
+						if (Input.GetKeyDown(KeyCode.Space))
+						{
+							MonoBehaviour.Destroy(talkUIImage.gameObject);
+							isTalkStart = false;
+							isLockCoolDown = true;
+							isOver = true;
+							readPerCharTimer = 0f;
+							lineIndex = 0;
+							index = 0;
+						}
+					}
+				}
+			}
+		}
+
+		if(isLockCoolDown)
+		{
+			lockCoolDownTimer += Time.deltaTime;
+			if(lockCoolDownTimer >= lockCoolDownMaxTime)
+			{
+				isLock = false;
+				isLockCoolDown = false;
+				lockCoolDownTimer = 0f;
+			}
+		}
+	}
+
+	public void SetTalk(string npcName,string[] talkData,Vector3 pos)
+	{
+		if (isLock)
+			return;
+
+		this.talkData = talkData;
+		isTalkStart = true;
+		isLock = true;
+		isOver = false;
+
+		var talk =  MonoBehaviour.Instantiate(talkBubblePrefab,Vector3.zero, Quaternion.identity);
+		talk.transform.parent = root.transform;
+		talk.transform.position = Camera.main.WorldToScreenPoint(pos);
+
+		talkUIImage = talk.GetComponent<Image>();
+		talkUIText = talk.transform.GetChild(0).GetComponent<Text>();
+		talkUIText.text = "";
+
+		talk.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = npcName;
+
+		Debug.Log(talk.transform.position);
+	}
+		
+}
